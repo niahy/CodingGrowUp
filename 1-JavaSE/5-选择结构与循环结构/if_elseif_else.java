6a63181ee8cc88f72e200ffe9bc814a0c5667741
Sumsurf <Sumsurf@outlook.com>
2026-01-04 00:19:45 +0800

    update ifelse结构的示例Java代码

---
+++ 1-JavaSE/5-选择结构与循环结构/if_elseif_else.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;//注意引入此包才能使用Scanner类，即进行键盘输入
+
+public class if_elseif_else{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);//创建Scanner对象，这是Java提供的固定的用法
+        System.out.println("输入你的成绩（整数）：");
+        int score = s.nextInt();
+        //这个"next+数据类型"的方法是Scanner类的方法，用于获取输入的数然后赋值给score变量
+
+        if (score >= 80){
+            System.out.println("成绩很优秀");
+        }else if(score >=60 && score < 80){
+            System.out.println("成绩及格");
+        }else{
+            System.out.println("成绩不及格");
+        }
+    }
+}
