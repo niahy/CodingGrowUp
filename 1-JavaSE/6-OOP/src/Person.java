5d2230053e5fb57914483de157a8ff8651a2a390
Sumsurf <Sumsurf@outlook.com>
2026-01-14 23:45:40 +0800

    更新类的示例代码

--- 1-JavaSE/6-OOP/src/Person.java
+++ 1-JavaSE/6-OOP/src/Person.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Person {
+    String name;//名字
+    int age;//年龄
+    double height;//身高
+
+    public void eat(){
+        System.out.println(name + "正在吃东西");
+    }
+    public void sleep(){
+        System.out.println(name + "正在睡觉");
+    }
+    public void show(){
+        System.out.println("姓名：" + name + "，年龄：" + age + "，身高：" + height);
+    }
 }
