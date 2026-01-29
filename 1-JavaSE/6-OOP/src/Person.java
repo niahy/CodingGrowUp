4b89421e1c924bbada97297358a4406b23b9e395
Sumsurf <Sumsurf@outlook.com>
2026-01-29 21:27:59 +0800

    更新构造方法

--- 1-JavaSE/6-OOP/src/Person.java
+++ 1-JavaSE/6-OOP/src/Person.java
@@ -3,6 +3,14 @@ public class Person {
     int age;//年龄
     double height;//身高
 
+    /*
+    //这是构造方法，相当于初始化对象的属性值。当然也可以在主类中创建对象时，给属性赋值
+    public Person(){
+        name = "张三";
+        age = 18;
+        height = 1.7;
+    }
+    */
     public void eat(){
         System.out.println(name + "正在吃东西");
     }
