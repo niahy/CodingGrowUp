b79910abc38576d68800d09024a2fd3dae66bf55
Sumsurf <Sumsurf@outlook.com>
2026-01-31 23:43:36 +0800

    类的组合

--- 1-JavaSE/6-OOP/src/person/PersonCar.java
+++ 1-JavaSE/6-OOP/src/person/PersonCar.java
@@ -2,6 +2,14 @@ package person;
 
 public class PersonCar {
     public static void main(String[] args) {
+        Person p = new Person();
+        Car c = new Car();
 
+        p.name = "顾客";
+        c.brand = "奥迪";
+
+        p.buyCar(c);
+        p.drive();
+        System.out.println(p.name + c.brand);
     }
 }
