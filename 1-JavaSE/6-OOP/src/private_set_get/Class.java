d20c308bcf9dd7d8b5ff09b5972b9f0e2e477353
Sumsurf <Sumsurf@outlook.com>
2026-02-01 17:52:40 +0800

    Class 演示private封装

---
+++ 1-JavaSE/6-OOP/src/private_set_get/Class.java
@@ -0,0 +1,15 @@
+package private_set_get;
+
+public class Class {
+    private String name;
+    private String type;
+    private int ID;
+    private int studentNum;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+}
