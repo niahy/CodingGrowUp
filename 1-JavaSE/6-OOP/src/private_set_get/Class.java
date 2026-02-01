ed11f792a388f6bec57646dbe51dfe3b107b6407
Sumsurf <Sumsurf@outlook.com>
2026-02-01 17:58:18 +0800

    Class中，使用快捷键alt+insert来自动生成所有属性的getset方法

--- 1-JavaSE/6-OOP/src/private_set_get/Class.java
+++ 1-JavaSE/6-OOP/src/private_set_get/Class.java
@@ -6,6 +6,30 @@ public class Class {
     private int ID;
     private int studentNum;
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getStudentNum() {
+        return studentNum;
+    }
+
+    public void setStudentNum(int studentNum) {
+        this.studentNum = studentNum;
+    }
+
     public void setName(String name){
         this.name = name;
     }
